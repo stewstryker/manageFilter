28f587b7c135e9feeaf950f5f944f6aaa53fac2f
Stew Stryker <sstryker58@gmail.com>
2022-12-01 10:23:27 -0500

    Convert IN separator to match IG requirement
    Using the new "IN separator" attribute, if the comparison operator is IN/Not IN, replace the separator with 01x, which is what the IG expects.

--- server/js/manageFilter.js
+++ server/js/manageFilter.js
@@ -11,8 +11,14 @@ var manageFilter = {
     var vCaseSensitive      = ( da.action.attribute08 == "Y" );  
     var vRefreshData        = ( da.action.attribute09 == "Y" );  
     var vSkipNoChangesCheck = ( da.action.attribute10 == "Y" );  
+    var vInSeparator        = ( da.action.attribute11 == "Y" );  
 
     var vRegion = apex.region( da.affectedElements[0].id );
+    
+    // If operator is IN or NOT IN, replace with provided value
+    if ((vInSeparator) && (vOperator == "IN" || vOperator == "NIN")) {
+      vValue = vValue.replace(/:/g, String.fromCharCode(01));
+    }
     
     // Remove current filters
     var vFilters = vRegion.call( "getFilters" );
